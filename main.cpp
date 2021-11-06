1eac44768d4d61754b48d881d750020922b8c0e6
Wiktor Piotrowski <s24894@szuflandia.pjwstk.edu.pl>
2021-11-06 16:22:18 +0100

    Pierwszy kod kalkulatora

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a, b, c;
+cout << "podaj 3 liczby";
+cin >> a >> b >> c;
+cout << "srednia wynosi: " << (a+b+c)/3 << endl;
+return 0;
+}
+
